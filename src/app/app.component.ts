e58b213719441b2d507fbb137c5513054f7311dd
alirezajs <alireza.varmaghani@gmail.com>
2022-02-06 14:55:12 +0300

    Fix some problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SOLID-Principle-in-Angular';
+  public onExport() {
+
+    console.log('export on json file')
+
+  }
 }
